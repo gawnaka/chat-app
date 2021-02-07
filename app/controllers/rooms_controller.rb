cf2fb4a17cb768d44027825f53040a252a484e28
gawnaka <gawnaka0828@gmail.com>
2021-02-07 21:52:42 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+
+  def index
+  end
+
   def new
     @room = Room.new
   end
@@ -10,7 +14,8 @@ class RoomsController < ApplicationController
     else
       render :new
     end
-
+  end
+  
     private
 
   def room_params
